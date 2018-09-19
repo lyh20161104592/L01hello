9ef35b71f14f52492fad8a275d9c74d81d8d2d32
20131104592 <lyh@admindeiMac-62.local>
2018-09-19 12:00:46 +0800

    3
    3

--- L01hello/MyPlayground.playground/Contents.swift
+++ L01hello/MyPlayground.playground/Contents.swift
@@ -2,9 +2,13 @@
 
 import UIKit
 //两个数字相加；1到100累加；10个数字排序；
-var a = 10
-var b = 20
-var sum = a + b
-print("a+b的结果是：\(sum)")
-
-
+//var shuzu:[Int] = [3,6,8,4,7,3,2,1,10,98]
+var sum = 0
+for i in 1...100
+{
+    sum = sum + i
+}
+print(sum)
+var array1:[Int]=[3,5,4,1,2,7,9,8,13,12]
+arry1.sort()
+print(array1)
