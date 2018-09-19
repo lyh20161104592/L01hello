97e894e73baa890344436f5dedb9e910999eda1b
20131104592 <lyh@admindeiMac-62.local>
2018-09-19 10:35:20 +0800

    2
    2

---
+++ L01hello/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//两个数字相加；1到100累加；10个数字排序；
+var a = 10
+var b = 20
+var sum = a + b
+print("a+b的结果是：\(sum)")
+
+
--- L01hello/main.swift
+++ L01hello/main.swift
@@ -15,13 +15,15 @@ var b = 2
 let c = a+b
 
 var str = "Hello "
+var w:String = ""
 var s:String = "world"
 var words:String = "neimenggushifandaxue"
 str = str + "shijie"//拼接
 str = "\(str),hello.\(100)"//插入
 str += "4592"
-
-
+if w.isEmpty{
+       print("w is empty String!")
+}
 
 var dict = ["name":"jjj","age":"89"]
 dict["sex"]="Female"
@@ -29,6 +31,9 @@ print(dict)
 print(dict["name"])
 print(str)
 print(c)
+
+for char in "我想去杭州"{
+       
 
 for x in 0...10{
        print("\(x) ",terminator: " ")
